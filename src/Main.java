bab16de59a3bb8f3c72acea5dbc612ee43debe2d
Asranov Baxtinur <asranov0003@gmail.com>
2023-09-07 10:24:59 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Bakhtnur");
+        System.out.println((user.name));
+        user.sayHello();
+    }
+}
