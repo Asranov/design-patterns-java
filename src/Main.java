265d1b48e5569ee68842c5677fd687b0dddf7b86
Asranov Baxtinur <asranov0003@gmail.com>
2023-09-07 10:54:04 +0500

    abstraction

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        var account = new Account();
-        account.deposid(100);
-        account.withdraw(50);
-        System.out.println(account.getBalance());
+        MailService mailService = new MailService();
+        mailService.sendEmail();
     }
 }
