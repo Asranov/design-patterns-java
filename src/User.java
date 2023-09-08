db65cc8959924b4c47a06df8aaf5391f476e9be2
Asranov Baxtinur <asranov0003@gmail.com>
2023-09-08 10:26:00 +0500

    addition user

--- src/User.java
+++ src/User.java
@@ -3,6 +3,7 @@
 public class User {
     //Field (attributes)
     public String name;
+    public Integer age;
 
     public User(String name) {
         this.name = name;
@@ -10,6 +11,6 @@ public class User {
 
     //Methods
     public void sayHello() {
-        System.out.println("Hi, my name is " + name);
+        System.out.println("Hi, my name is " + name + "i am " + age + "years old");
     }
 }
